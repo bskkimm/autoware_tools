8a5a02f5558ae7dac5beea1c62997cce4fa5d7e0
beomseok-kimm <beomseok.kim.2@tier4.jp>
2026-05-29 13:59:54 +0900

    backup: preserve score root include edit
    Signed-off-by: beomseok-kimm <beomseok.kim.2@tier4.jp>

--- planning/autoware_planning_data_analyzer/src/metrics/trajectory_metrics.cpp
+++ planning/autoware_planning_data_analyzer/src/metrics/trajectory_metrics.cpp
@@ -21,6 +21,7 @@
 #include "metrics/epdms/subscores/traffic_light_compliance.hpp"
 #include "metrics/epdms/subscores/ttc_within_bound.hpp"
 #include "metrics/geometry/ego_footprint.hpp"
+#include "metrics/geometry/lanelet_geometry.hpp"
 #include "metrics/geometry/lanelet_queries.hpp"
 #include "metrics/geometry/metric_utils.hpp"
 
